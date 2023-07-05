7eec0cfc4e8989af8bf6a0f20f2b5126fc1de593
Ghost <OverlyGenericAddress@pm.me>
2023-07-05 19:14:35 +0200

    Finished activities page

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import streamlit as st
 from datetime import datetime
-from pages.activities import activities_page
-from pages.rate import rate_page
-from pages.write import write_page
+from activities import activities_page
+from rate import rate_page
+from write import write_page
 
 # Set config to always start with wide layout
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", page_title="My Diary")
 
 # Main title
 st.title('Today is the {} welcome to your digital diary!'
          .format(datetime.today().strftime('%d.%m.%Y')))
 
+# Split application into tabs
 activities, rate, write = st.tabs(
     ["Specify your activities...", "Rate your day!", "Wanna mention something else?"])
 
+# Import tabs content/functionalities
 with activities:
+    # Holds information about the daily activities
     activities_page()
 
 with rate:
+    # Used to judge the day
     rate_page()
 
 with write:
+    # Used to specify additional information
     write_page()
