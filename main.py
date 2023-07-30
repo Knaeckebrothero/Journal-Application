b65776b7aaa3cd9cc795ad137e7104e4fe059fe1
Ghost <OverlyGenericAddress@pm.me>
2023-07-30 12:53:45 +0200

    Updated the activities page

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import streamlit as st
-from datetime import datetime as dt, time 
-
+from datetime import datetime as dt, time
 
 # Config & initialisation
 st.set_page_config(layout="wide", page_title="My Diary")
 today = {"date": dt.today(),
          "activity": [],
-         "ratings": {},
+         "rating": {},
          "comment": {},
          }
 
@@ -16,24 +15,29 @@ st.title('Today is the {} welcome to your digital diary'
 
 # Split application into tabs
 activities, rate, write, organize = st.tabs(
-    ["What did you do today?", "Rate your day!", "Wanna mention something?", "Manage your diary."])
+    ["What did you do today?", "Rate your day!", "Wanna mention something?", "Manage your diary's..."])
 
 # Import tabs content/functionalities
 with activities:
+    # Specify how demanding the activity was.
+    activity_category = st.selectbox('Tag based on cognitive demand',
+                                     ['Concentration', 'Relaxation', 'Organizational'],
+                                     index=2, key='activity_category')
+
     # Enter information about the activity
-    activity_name = st.text_input(
-        'Activity', key='activity_name', value="", placeholder='What did you do?')
+    activity_description = st.text_input(
+        'Describe your activity', key='activity_description', value="", placeholder='What did you do?')
     activity_start = st.time_input(
-        'Start time', key='activity_start', value=time(0,0), disabled=False)
+        'Start time', key='activity_start', value=time(0, 0), disabled=False)
     activity_end = st.time_input(
-        'End time', key='activity_end', value=time(0,0), disabled=False)
+        'End time', key='activity_end', value=time(0, 0), disabled=False)
 
     # Tag your activity
-    activity_tag = st.selectbox('Tag your activity',
-                                ['Academic', 'Work', 'Personal',
-                                 'Relaxation', 'Free time', 'Social',
-                                 'Sport', 'Housekeeping',
-                                 'Other'], index=8)
+    activity_tags = st.multiselect("Activity was related to",
+                                   options=['Academic', 'Business', 'Hobbies',
+                                            'Breaks', 'Leisure', 'Exercise',
+                                            'Housekeeping', 'Personal Care', 'Administrative'],
+                                   key='activity_tags')
 
     # Spacing to make it look nicer
     st.markdown('<br/>', unsafe_allow_html=True)
@@ -41,10 +45,11 @@ with activities:
     # Button to add a new activity
     if st.button("Add activity"):
         today["activity"].append(
-            {"activity": activity_name,
-                "start": activity_start,
-                "end": activity_end,
-                "tag": activity_tag})
+            {"description": activity_description,
+             "start": activity_start,
+             "end": activity_end,
+             "category": activity_category,
+             "tags": activity_tags})
 
 with rate:
     # Sliders and diagram
@@ -69,7 +74,8 @@ with rate:
         focus_comment = st.text_input("Did you feel stressed?", key='focus_comment')
         st.markdown('<br/>', unsafe_allow_html=True)
         start_mood_comment = st.text_input("What was responsible for this?", key='start_mood_comment')
-        end_mood_comment = st.text_input("What should i write here?", key='end_mood_comment', label_visibility = 'hidden')
+        end_mood_comment = st.text_input("What should i write here?", key='end_mood_comment',
+                                         label_visibility='hidden')
         st.markdown('<br/>', unsafe_allow_html=True)
         satisfaction_comment = st.text_input("What would others say?", key='satisfaction_comment')
 
@@ -102,6 +108,6 @@ with organize:
         pass
 
     # Select date for diary entry
-    today = st.text_input('If you want to change the date please do so, if not simply continue...', 
+    today = st.text_input('If you want to change the date please do so, if not simply continue...',
                           value=dt.today().strftime('%d.%m.%Y'),
                           max_chars=None, key=None, type='default')
