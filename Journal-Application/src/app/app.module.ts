cee33f0db2b61aba57a9bfeb47bce43b9cdb3f4b
Ghost <OverlyGenericAddress@pm.me>
2023-12-24 17:18:56 +0100

    Done setting up frontend appshell

--- Journal-Application/src/app/app.module.ts
+++ Journal-Application/src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { RouterModule, Routes } from '@angular/router';
-import { ActivitiesComponent } from './landingpage/landingpage.component';
+import { ActivitiesComponent } from './activities/activities.component';
 import { NavigationBarComponent } from './navbar/navigation-bar.component';
 import { HeadBarComponent } from './head-bar/head-bar.component';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { RateComponent } from './rate/rate.component';
+import { CommentComponent } from './comment/comment.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Routes
 const routes: Routes = [
-  { path: '/activities', component: ActivitiesComponent },
-  { path: '**', redirectTo: '/activities' }
+  { path: 'activities', component: ActivitiesComponent },
+  { path: 'rate', component: RateComponent },
+  { path: 'comment', component: CommentComponent }
 ];
 
 @NgModule({
@@ -24,6 +28,8 @@ const routes: Routes = [
     NavigationBarComponent,
     ActivitiesComponent,
     HeadBarComponent,
+    RateComponent,
+    CommentComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,7 +42,8 @@ const routes: Routes = [
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
